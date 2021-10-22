6083c32a3702c33001a3a1b41d63f3ae02e55105
fbmassi <90656227+fbmassi@users.noreply.github.com>
2021-10-22 18:19:10 -0300

    Create main.py

---
+++ Consola/main.py
@@ -0,0 +1,17 @@
+# PROYECTO FINAL PYTHON INICIAL 
+# AUTOR: FRANCO BALTASAR MASSI
+# BUSCADOR DE ALQUILERES
+
+def ingreso():
+    #Solicita el ingreso de un valor y devuelve el mismo. 
+    ingreso = input("Ingrese: ")
+    return ingreso 
+
+    
+def main():
+    #Funcion principal del programa. 
+    print("BIENVENIDO AL BUSCADOR DE ALQUILERES")
+    print("AQUI USTED PODRA ENCONTRAR LOS MEJORES DEPARTAMENTOS EN ALQUILER EN LA CIUDAD \nDE MENDOZA, CON LAS CARACTERISTICAS QUE USTED DESEE.")
+    #
+main()
+
