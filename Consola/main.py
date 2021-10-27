533b37808d51d4d9df31f99e2fec7de5ded67292
fbmassi <90656227+fbmassi@users.noreply.github.com>
2021-10-27 18:10:04 -0300

    Update main.py

--- Consola/main.py
+++ Consola/main.py
@@ -1,11 +1,21 @@
 # PROYECTO FINAL PYTHON INICIAL 
 # AUTOR: FRANCO BALTASAR MASSI
 # BUSCADOR DE ALQUILERES
-    
+
+from other_functions import desea_seguir
+
+
 def main():
-    #Funcion principal del programa. 
+    """Funcion principal del programa. A travez de esta, corre el programa general.
+    Le muestra al usuario los resyltados de su busqueda."""
+
+    #propiedades = open("propiedades.csv")
+    #resultados_de_busqueda = ("resultados_de_busqueda.csv, "w")
     print("BIENVENIDO AL BUSCADOR DE ALQUILERES")
-    print("AQUI USTED PODRA ENCONTRAR LOS MEJORES DEPARTAMENTOS EN ALQUILER EN LA CIUDAD \nDE MENDOZA, CON LAS CARACTERISTICAS QUE USTED DESEE.")
-    #
+    print("AQUI USTED PODRA ENCONTRAR LOS MEJORES DEPARTAMENTOS EN ALQUILER EN LA CIUDAD \nQUE USTED ELIJA, CON LAS CARACTERISTICAS QUE USTED DESEE.")
+    #while desea_seguir():
+    #   extraer_datos(propiedades, resultados_de_busqueda)
+    #    print("S")
+    print("MUCHAS GRACIAS POR HABER USADO ESTA PLATAFORMA, VUELVA PRONTO.")
 main()
 
