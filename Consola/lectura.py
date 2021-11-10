6e85026a0bcde6cf3aaaa8fc1014f5195c0b51fb
fbmassi <90656227+fbmassi@users.noreply.github.com>
2021-11-10 01:21:18 -0300

    CONSTRUCCION2.0

--- Consola/lectura.py
+++ Consola/lectura.py
@@ -1,10 +1,10 @@
 #IMPORTACIONES
-from other_functions import validar_ingreso
-from other_functions import validar_numero
-from other_functions import leer
+from other_functions import validar_ingreso, validar_numero, leer
 
 #CONSTANTES
-(NUMERO, FECHA, LATITUD, LONGITUD, URL, TITULO, TIPO_DE_PROPIEDAD, PRECIO, MONEDA, SUPERFICIE, AMBIENTES) = (0,1,2,3,4,5,6,7,8,9,10) 
+(NUMERO, FECHA, LATITUD, LONGITUD,
+ URL, TITULO, TIPO_DE_PROPIEDAD, 
+ PRECIO, MONEDA, SUPERFICIE, AMBIENTES) = (0,1,2,3,4,5,6,7,8,9,10) 
 (PRINCIPIO, FIN) = (0,2) 
 
 #FUNCIONES
@@ -34,9 +34,41 @@ def contar_propiedades(propiedades):
     return contador
 
 
-def superficie(propiedades, archivo_filtrado):
+def filtros_a_aplicar(filtros_deseados):
+    
+    
+    desea_ambientes =  validar_ingreso(input("Desea filtrar la busqueda por cantidad de ambientes de la propiedad? (Ingrese S/N): ").upper())
+    
+    desea_superficie =  validar_ingreso(input("Desea filtrar la busqueda por la superficie de la propiedad? (Ingrese S/N): ").upper())
+        
+    desea_divisa =  validar_ingreso(input("Desea filtrar la busqueda por divisa en la que se tasa de la propiedad? (Ingrese S/N): ").upper())
+        
+    desea_precio =  validar_ingreso(input("Desea filtrar la busqueda por el precio de la propiedad? (Ingrese S/N): ").upper())
+        
+    desea_t_propiedad =  validar_ingreso(input("Desea filtrar la busqueda por el tipo de propiedad? (Ingrese S/N): ").upper())
+    
+    
+    if desea_ambientes:
+        filtros_deseados += [AMBIENTES]
+    if desea_superficie:
+        filtros_deseados += [SUPERFICIE]
+    if desea_divisa:
+        filtros_deseados += [MONEDA]
+    if desea_precio:
+        filtros_deseados += [PRECIO]
+    if desea_t_propiedad:
+        filtros_deseados += [TIPO_DE_PROPIEDAD]
 
-    propiedad = leer(propiedades)
+    
+    return filtros_deseados
+
+
+def ambientes(propiedad):
+
+
+
+def superficie(propiedad):
+
 
     min_superficie_deseada = validar_numero(input("Ingrese la minima superficie que desea para su propiedad: "))
 
@@ -53,31 +85,68 @@ def superficie(propiedades, archivo_filtrado):
 
    
 
+    if propiedad[SUPERFICIE] >= min_superficie_deseada and propiedad[SUPERFICIE] <= max_superficie_deseada:
+
+        propiedad += [SUPERFICIE]
+
+    return propiedad
+
+def divisa(propiedad):
+
+def precio(propiedad):
+
+def tipo_propiedad(propiedad):
+
+
+
+def aplicar_filtros(propiedades, busqueda, filtros_deseados):
+
+    propiedad = leer(propiedades)
+
+    propiedad = leer(propiedades)
+
+    NUMERO_DE_FILTROS = len(filtros_deseados)
+
+
     while propiedad[NUMERO]:
 
 
-        if propiedad[SUPERFICIE] >= min_superficie_deseada and propiedad[SUPERFICIE] <= max_superficie_deseada:
+        if NUMERO_DE_FILTROS > 0:
             
-            archivo_filtrado.write(propiedad[TITULO] + "," + propiedad[TIPO_DE_PROPIEDAD] + "," + propiedad[MONEDA] + "," + propiedad[PRECIO] + "," + propiedad[AMBIENTES] + "," + propiedad[SUPERFICIE] + "," + propiedad[URL] + "\n")
+            if AMBIENTES in filtros_deseados:
+                 
+                 propiedad = ambientes(propiedad)
+
+            if SUPERFICIE in filtros_deseados:
+                
+                propiedad = superficie(propiedad)
+                
+            if MONEDA in filtros_deseados:
+                
+                propiedad = divisa(propiedad)
+                
+            if PRECIO in filtros_deseados:
+                
+                propiedad = precio(propiedad)
+                
+            if TIPO_DE_PROPIEDAD in filtros_deseados:
+                
+                propiedad = tipo_propiedad(propiedad)
 
 
-#def rango_de_precios(propiedades, resultados_de_busqueda):
+        if propiedad[11: 11 + NUMERO_DE_FILTROS] == filtros_deseados:
 
-#    """
-#    Esta funcion analiza si un elemento del archivo 
-#    del que se extrae la informacion,
-#    cumple con requisitos ingresados por el usuario.
-#    """
+            #CORREGIR!!!!!!
+            # busqueda.write(propiedad[TITULO] + "," + propiedad[TIPO_DE_PROPIEDAD] + "," + propiedad[MONEDA] + "," + propiedad[PRECIO] + "," + propiedad[AMBIENTES] + "," + propiedad[SUPERFICIE] + "," + propiedad[URL] + "," + propiedad[LATITUD] +"," + propiedad[LONGITUD] +"\n")
 
-#    propiedad = leer(propiedades)
+            propiedad = leer(propiedades)
 
-#"""
+        else:
+            propiedad = leer(propiedades)
 
-#"""
 
-def extraer_datos(propiedades, resultados_de_busqueda):
 
-    propiedad = leer(propiedades)
+def analizar_datos(propiedades, busqueda):
 
     print("GRACIAS POR ELEGIRNOS.")
 
@@ -85,7 +154,11 @@ def extraer_datos(propiedades, resultados_de_busqueda):
 
     desea_filtros = validar_ingreso(input("Desea realizar filtros en su busqueda? (Ingrese S/N").upper())
 
+    filtros_deseados = []
+
     if not desea_filtros:
+
+        propiedad = leer(propiedades)
 
         propiedad = leer(propiedades)
 
@@ -95,7 +168,27 @@ def extraer_datos(propiedades, resultados_de_busqueda):
 
             propiedad = leer(propiedades)
     else:
-        desea_superficie =  validar_ingreso(input("Desea filtrar la busqueda por la suoerficie de la propiedad? (Ingrese S/N): ").upper())
+
+        filtros_deseados = filtros_a_aplicar(filtros_deseados)
+
+        aplicar_filtros(propiedades, busqueda, filtros_deseados)
+
+
+
+def mostrar_resultados(resultados_de_busqueda):
+
+    print("HAY", contar_propiedades(resultados_de_busqueda), " PROPIEDADES A SUS PREFERENCIAS.")
+
+    propiedad = leer(resultados_de_busqueda)
+
+    while propiedad[NUMERO]:
+
+        #CORREGIR!!!!!!!!
+        # print("\n" + "\n" + "Tipo de propiedad: " + propiedad[TIPO_DE_PROPIEDAD]  + "\n" + "Titulo: " + propiedad[TITULO] + "\n" + "Ubicacion----  Latitud: " + propiedad[LATITUD] + "  Longitud:" + propiedad[LONGITUD] + "\n" + "Moneda: " + propiedad[MONEDA] + "\n" + "Precio: " + propiedad[PRECIO] + "\n" + "Ambientes: " + propiedad[AMBIENTES] + "\n" + "Superficie: "  + propiedad[SUPERFICIE] + "\n" + "URL: " + propiedad[URL])
+
+        propiedad = leer(resultados_de_busqueda)
+
+
 
 
 
@@ -103,3 +196,6 @@ def extraer_datos(propiedades, resultados_de_busqueda):
 #lista = [x.upper() for x in ["numero", "fecha", "latitud" , "longitud", "url", "titulo", "tipo de propiedad", "precio" ,"moneda" , "SUPERFICIE", "ambientes"]]
 #print(lista)
 
+#if propiedad[SUPERFICIE] >= min_superficie_deseada and propiedad[SUPERFICIE] <= max_superficie_deseada:
+            
+#busqueda.write(propiedad[TITULO] + "," + propiedad[TIPO_DE_PROPIEDAD] + "," + propiedad[MONEDA] + "," + propiedad[PRECIO] + "," + propiedad[AMBIENTES] + "," + propiedad[SUPERFICIE] + "," + propiedad[URL] + "," + propiedad[LATITUD] +"," + propiedad[LONGITUD] +"\n")
--- Consola/main.py
+++ Consola/main.py
@@ -2,8 +2,7 @@
 # AUTOR: FRANCO BALTASAR MASSI
 # BUSCADOR DE ALQUILERES
 
-from other_functions import desea_seguir
-from lectura import contar_propiedades
+from lectura import contar_propiedades, analizar_datos, mostrar_resultados
 
 def main():
 
@@ -12,12 +11,14 @@ def main():
     Le muestra al usuario los resyltados de su busqueda.
     """
 
-    #propiedades = open("propiedades.csv")
-    #resultados_de_busqueda = ("resultados_de_busqueda.csv, "w")    print("BIENVENIDO AL BUSCADOR DE ALQUILERES")
+    #propiedades = open("propiedades_CABA.csv")
+    #busqueda = ("busqueda.csv", "w")    print("BIENVENIDO AL BUSCADOR DE ALQUILERES")
     print("AQUI USTED PODRA ENCONTRAR LOS MEJORES DEPARTAMENTOS EN ALQUILER EN LA CIUDAD \nQUE USTED ELIJA, CON LAS CARACTERISTICAS QUE USTED DESEE.")
-    #while desea_seguir():
-    #   extraer_datos(propiedades, resultados_de_busqueda)
-    #  print("S")
+    #analizar_datos(propiedades, busqueda)
+    #propiedades.close()
+    #busqueda.close()
+    #resultados_de_busqueda = open("busqueda.csv")
+    #mostrar_resultados(resultados_de_busqueda):
     print("MUCHAS GRACIAS POR HABER USADO ESTA PLATAFORMA, VUELVA PRONTO.")
 
     
